663370ad2b6e7cf57a60efbc2a72027ce9112198
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-03-08 17:07:26 -0300

    Classes Cor e Peca implementadas, e a Classe Tabuleiro começando a ser implementada

---
+++ xadrez-console/xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        private Peca[,] _pecas;
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+            _pecas=new Peca[linha, coluna];
+        }
+    }
+}
