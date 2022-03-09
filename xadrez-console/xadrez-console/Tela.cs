a3f8c8546e159e5e8fc096c98b05a057be218b34
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-03-08 23:35:03 -0300

    Mudando a cor das peças pretas

--- xadrez-console/xadrez-console/Tela.cs
+++ xadrez-console/xadrez-console/Tela.cs
@@ -5,10 +5,11 @@ namespace xadrez_console
 {
     internal class Tela
     {
-        public static void imprimirTabuleiro(Tabuleiro tab)
+        public static void ImprimirTabuleiro(Tabuleiro tab)
         {
             for(int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write($"{8-i} ");
                 for(int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.Peca(i, j) == null)
@@ -17,10 +18,26 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.Peca(i, j) + " ");
+                        Tela.ImprimirPeca(tab.Peca(i,j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f h j");
+        }
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
