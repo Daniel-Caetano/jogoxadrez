cfee134ec58c77025b62246c2db3ba223e281028
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-03-08 22:43:23 -0300

    Criando peças torre e rei e colocando no tabuleiro

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -7,10 +8,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3,4);
-
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.ColocarPeca(new Torre( tab, Cor.Preta), new Posicao(0,0) );
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
             Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
--- xadrez-console/xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -12,10 +12,15 @@
             this.Coluna = coluna;
             _pecas=new Peca[linha, coluna];
         }
-        public Peca peca(int linha, int coluna)
+        public Peca Peca(int linha, int coluna)
         {
             return _pecas[linha, coluna];
         }
 
+        public void ColocarPeca(Peca p,Posicao pos)
+        {
+            _pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+        }
     }
 }
