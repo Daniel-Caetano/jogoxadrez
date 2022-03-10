7882bd54b5c96e74c61f5a5f6e2dc9d5cd1d7f0c
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-03-10 15:37:48 -0300

    Mudando o nome do metodo PodeMoverPara para o nome Movimento Possivel, da Classe Peca

--- xadrez-console/xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/xadrez-console/Tabuleiro/Peca.cs
@@ -37,7 +37,7 @@
             return false;
         }
 
-        public bool PodeMoverPara(Posicao posicao)
+        public bool MovimentoPossivel(Posicao posicao)
         {
             return MovimentosPossiveis()[posicao.Linha, posicao.Coluna];
         }
